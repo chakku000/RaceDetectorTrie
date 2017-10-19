990eeebc12e68360d88270253746781272836108
chakku <chakkurai0724@outlook.jp>
2017-10-19 19:00:09 +0900

    fix

--- Trie.hpp
+++ Trie.hpp
@@ -132,25 +132,3 @@ class Trie{
         }
 };
 
-
-/*
-int main(){
-    Trie<int,int> trie;
-
-    std::set<int> st1{1,2,3,4};
-    std::set<int> st2{1,2,5};
-    std::set<int> st3{1,2,3};
-    trie.insert(st1,1,WRITE);
-    trie.insert(st2,2,WRITE);
-    trie.insert(st3,3,WRITE);
-
-    TrieViewer::view<int,int>(trie,"out.dot");
-    TrieViewer::view<int,int>(trie,"out2.dot");
-
-    std::set<int> new_access{1,2,3,4};
-
-    bool f = trie.hasWeaknessAccess(new_access,WRITE,3);
-
-    std::cout << std::boolalpha << f << std::endl;
-}
-*/
